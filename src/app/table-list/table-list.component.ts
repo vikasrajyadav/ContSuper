792fa7260f2ed4199c4971bcbb51bf092891dee7
aditya sawant <sawant.adityan@gmail.com>
2019-07-28 16:10:51 +0530

    table data

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -8,85 +8,249 @@ import { Router } from '@angular/router';
 })
 export class TableListComponent implements OnInit {
 
-  activityList: any = [{
+  activityList: any = [
+    {
     id: 1,
     srNo: 1,
-    description: 'abc',
+    description: 'Removing of manglore tiles from roof',
     compStatus: '3%',
     estimated: 0
   }, {
     id: 2,
     srNo: 2,
-    description: 'abc',
+    description: 'Removing of POP false ceiling',
     compStatus: '5%',
     estimated: 0
   }, {
     id: 3,
     srNo: 3,
-    description: 'abc',
+    description: 'Breaking and removing Brick work',
     compStatus: '5%',
     estimated: 0
   }, {
     id: 4,
     srNo: 4,
-    description: 'abc',
+    description: 'Removing existing doors, frames, shutter and window, frame, shutter',
     compStatus: '4%',
     estimated: 0
   }, {
     id: 5,
     srNo: 5,
-    description: 'abc',
+    description: 'Filling rabit in plinth and leveling',
     compStatus: '100%',
     estimated: 0
   }, {
     id: 6,
     srNo: 6,
-    description: 'abc',
+    description: 'Soling in plinth and around the structure.',
     compStatus: '48%',
     estimated: 0
   }, {
     id: 7,
     srNo: 7,
-    description: 'abc',
+    description: 'Excavation for foundation',
     compStatus: '34%',
     estimated: 0
   }, {
     id: 8,
     srNo: 8,
-    description: 'abc',
+    description: 'Providing and constructing 230 mm thick brick wall',
     compStatus: '12%',
     estimated: 0
   }, {
     id: 9,
     srNo: 9,
-    description: 'abc',
+    description: 'Providing and constructing 150 mm thick brick wall',
     compStatus: '54%',
     estimated: 0
   }, {
     id: 10,
     srNo: 10,
-    description: 'abc',
+    description: 'Providing and applying 12 mm thk. Internal Plaster',
     compStatus: '65%',
     estimated: 0
   }, {
     id: 11,
     srNo: 11,
-    description: 'abc',
+    description: 'Providing and applying 20 mm thk. External Plaster',
     compStatus: '73%',
     estimated: 0
   }, {
     id: 12,
     srNo: 12,
-    description: 'abc',
+    description: 'Providing & applying two coat of painting',
     compStatus: '32%',
     estimated: 0
-  }, {
+  },
+   {
+    id: 13,
+    srNo: 13,
+    description: 'Providing and Applying two coat of Asian Paints Apex ultima to external walls',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 14,
+    srNo: 14,
+    description: 'Providing and laying 2’-0” X 2’-0” 10 mm thick Vitrified tiles',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 15,
+    srNo: 15,
+    description: 'Dado (Ceramic tiles in toilet & Kitchen)',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 16,
+    srNo: 16,
+    description: 'Providing and fixing Machine cut Machine Polished Kotah Stone in staircase',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 17,
+    srNo: 17,
+    description: 'Providing and fixing Machine polished machine Kotah Treads',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 18,
+    srNo: 18,
+    description: 'Ledge',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 19,
+    srNo: 19,
+    description: 'Providing and laying waterproofing treatment',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 20,
+    srNo: 20,
+    description: 'Flooring. (Anti-Skid for terrace)',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
     id: 13,
     srNo: 13,
     description: 'abc',
     compStatus: '98%',
     estimated: 0
-  }]
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+   ,{
+    id: 13,
+    srNo: 13,
+    description: 'abc',
+    compStatus: '98%',
+    estimated: 0
+  }
+]
 
   constructor(private router: Router) { }
 
